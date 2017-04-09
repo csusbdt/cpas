6ff6352915ed17853f63dcd00b2201c7192b87dd
David Turner <csusbdt@gmail.com>
2017-04-09 05:57:31 -0700

    x

---
+++ wiki.py
@@ -0,0 +1,58 @@
+# Convert INDIR/*.md to OUTDIR/*.html and copy 
+# github-markdown stylesheet to OUTDIR.
+
+import markdown
+import codecs
+from pathlib import Path
+from shutil import copy2 as copyfile
+INDIR = "../cpas.wiki/"
+OUTDIR = "temp/"
+
+header = '''<html>
+  <head>
+    <title>website</title>
+    <!-- stylesheet from http://www.bootcdn.cn/github-markdown-css/ -->
+    <link rel="stylesheet" href="github-markdown.css">
+    <style>
+      .markdown-body {
+        box-sizing: border-box;
+        min-width: 200px;
+        max-width: 980px;
+        margin: 0 auto;
+        padding: 45px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class='markdown-body'>
+      <h1>CSE 1010</h1>
+'''
+
+footer = '''
+    </div>
+  </body>
+</htlm>
+'''
+
+md = markdown.Markdown([
+    "markdown.extensions.wikilinks(base_url=, end_url=.html)", 
+    "markdown.extensions.tables",
+    "markdown.extensions.fenced_code"]
+)
+
+def createPage(filename):
+    inpath = INDIR + filename + ".md"
+    outpath = OUTDIR + filename + ".html"
+    outfile = codecs.open(outpath, "w", encoding="utf-8", errors="xmlcharrefreplace")
+    infile = codecs.open(inpath, mode="r", encoding="utf-8")
+    intext = infile.read()
+    outtext = header + md.convert(intext) + footer
+    outfile.write(outtext)
+
+wikiPagesDirectory = Path("../cpas.wiki")
+for x in wikiPagesDirectory.iterdir():
+    if not x.is_dir():
+        createPage(x.stem)
+
+copyfile("github-markdown.css", "temp")
+
