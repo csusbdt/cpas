d8d1c94ccf08f36ea191f6999744221b1560bfc6
David Turner <csusbdt@gmail.com>
2017-04-18 23:30:17 -0700

    X

--- wiki.py
+++ wiki.py
@@ -16,50 +16,19 @@ from shutil import copy2 as copyfile
 INDIR = "../cpas.wiki/"
 OUTDIR = "docs/"
 
-header = '''<html>
-  <head>
-    <title>CSE 1010</title>
-
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-      <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-      <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-
-    <!-- stylesheet from http://www.bootcdn.cn/github-markdown-css/ -->
-    <link rel="stylesheet" href="github-markdown.css">
-    <style>
-      .markdown-body {
-        box-sizing: border-box;
-        min-width: 200px;
-        max-width: 850px; /* 980px; */
-        margin: 0 auto;
-        padding: 45px;
-      }
-      .menu {
-        padding-bottom: .3em;
-        border-bottom: 1px solid #eaecef;
-      }
-    </style>
-  </head>
-  <body>
-    <div class='markdown-body'>
-      <div class="menu">
-        <span>CSE 1010</span> &bull;
-        <a href="Home.html">Home</a> &#9702; 
-        <a href="https://github.com/csusbdt/cpas/wiki">Wiki</a> <br>
-      </div>
-'''
-
-footer = '''
-    </div>
-  </body>
-</htlm>
-'''
-
 md = markdown.Markdown([
     "markdown.extensions.wikilinks(base_url=, end_url=.html)", 
     "markdown.extensions.tables",
     "markdown.extensions.fenced_code"]
 )
+
+template = ''
+with open('template.html', 'r') as f:
+    template = f.read()
+
+def embedIntoTemplate(content):
+    result = template.replace('[[content]]', content)
+    return result
 
 def createPage(filename):
     inpath = INDIR + filename + ".md"
@@ -67,7 +36,12 @@ def createPage(filename):
     outfile = codecs.open(outpath, "w", encoding="utf-8", errors="xmlcharrefreplace")
     infile = codecs.open(inpath, mode="r", encoding="utf-8")
     intext = infile.read()
-    outtext = header + md.convert(intext) + footer
+
+    content = md.convert(intext) 
+
+    #outtext = embedIntoTemplate(outtext)
+    outtext = template.replace('[[content]]', content)
+
     outfile.write(outtext)
 
 print("(1) Converting " + INDIR + "*.md to HTML and placing into " + OUTDIR)
